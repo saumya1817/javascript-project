247c965dd2135097df23aaf2c3b29bff82e7d57a
Saumya Singh <saumya.singh1817@gmail.com>
2024-01-17 09:30:38 +0530

    setup for intro page

--- awwwards-portfolio/script.js
+++ awwwards-portfolio/script.js
@@ -18,3 +18,29 @@ to("#fs",{
     delay: -1.4,
     ease: Expo.easeInOut
 })
+
+document.querySelectorAll(".reveal").forEach(function(elem){
+
+    //Create two span elements
+    let spanParent = document.createElement("span");
+    let spanChild = document.createElement("span");
+
+    //Add class to span elements
+    spanParent.classList.add("parent");
+    spanChild.classList.add("child");
+
+    //Assign h1 text to span child
+    spanChild.textContent = elem.textContent;
+
+    //Establish parent child relation
+    spanParent.appendChild(spanChild);
+    elem.spanChild(spanParent);
+
+    spanChild.style.display = "block";
+    spanChild.style.overflow = "hidden";
+    spanChild.style.top = "50%";
+    spanChild.style.left = "50%";
+
+    spanChild.style.backgroundColor = "red"
+
+});
